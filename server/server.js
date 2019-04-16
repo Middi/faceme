fd422ef934093001d4e54b381cb4cb08fb0103bb
Richard Middleton <richard@richardmiddleton.me>
2019-04-16 13:21:00 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const ss = require('socket.io-stream');
+const path = require('path');
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const fs = require('fs');
+
+io.on('connection', socket => {
+    console.log('connection');
+    socket.emit('connected', 'hello World');
+
+    ss(socket).on('file', (stream, data) => {
+        stream.pipe(fs.createWriteStream( Date.now() +'.txt'));
+    });
+
+    ss(socket).on('video', (stream, data) => {
+        stream.pipe(fs.createWriteStream( 'video' + Date.now() +'.txt'));
+    });
+
+});
+
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+server.listen(3000);
